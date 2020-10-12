33abb6eee024ff151fe11452edc5e742c4876040
Xiao-Lii <63008453+Xiao-Lii@users.noreply.github.com>
2020-10-12 14:17:40 -0600

    Base of IntelliJ Project

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+/*
+
+ This company wants a system that allows their employees to manage the products offered in the store as well
+ the users and product categories. Moreover, they want an application for their customers to browse their catalog,
+ search for products and make orders.
+
+
+
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}           // END OF MAIN
+
+
